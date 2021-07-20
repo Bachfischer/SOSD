30528f6a24e71f34bce48df41cf0fb56c13f036a
Matthias Bachfischer <bachfischer.matthias@googlemail.com>
2021-07-20 21:21:10 +0200

    update scripts to sample data

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -11,18 +11,15 @@ make -j 8
 
 function generate_lookups() {
     echo "Generating lookups for $1"
-    [ -f ../data/$1_equality_lookups_10M ] || ./generate ../data/$1 10000000
+    [ -f ../data/$1_equality_lookups_18M ] || ./generate ../data/$1 18000000
+    [ -f ../data/$1_equality_lookups_2M ] || ./generate ../data/$1 2000000
     [ -f ../data/$1_equality_lookups_1M ] || ./generate ../data/$1 1000000
-}
-
-function generate_lookups_test() {
-    echo "Generating lookups for $1"
-    [ -f ../data/$1_equality_lookups_500 ] || ./generate ../data/$1 500
 }
 
 function generate_inserts() {
     echo "Generating inserts for $1"
-    #[ -f ../data/$1_inserts_10M ] || ./generate ../data/$1 10000000 1
+    [ -f ../data/$1_inserts_18M ] || ./generate ../data/$1 18000000 1
+    [ -f ../data/$1_inserts_2M ] || ./generate ../data/$1 2000000 1
     [ -f ../data/$1_inserts_1M ] || ./generate ../data/$1 1000000 1
  } &> /dev/null
 
@@ -39,20 +36,20 @@ echo "Generating queries..."
 #generate_lookups uniform_sparse_200M_uint32
 #generate_lookups uniform_sparse_200M_uint64
 
-#generate_lookups osm_cellids_200M_uint64
+generate_lookups osm_cellids_200M_uint64
 #generate_lookups osm_cellids_400M_uint64
 #generate_lookups osm_cellids_600M_uint64
 #generate_lookups osm_cellids_800M_uint64
 
-#generate_lookups wiki_ts_200M_uint64
+generate_lookups wiki_ts_200M_uint64
 
 #generate_lookups books_200M_uint32
-#generate_lookups books_200M_uint64
+generate_lookups books_200M_uint64
 #generate_lookups books_400M_uint64
 #generate_lookups books_600M_uint64
 #generate_lookups books_800M_uint64
 
-#generate_lookups fb_200M_uint64
+generate_lookups fb_200M_uint64
 #generate_lookups_test uniform_dense_1K_uint64
 
 

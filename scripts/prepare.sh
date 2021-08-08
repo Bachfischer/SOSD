8680c83a9472255c776bce0b8db1930a5070f1e4
Matthias Bachfischer <bachfischer.matthias@googlemail.com>
2021-08-08 09:40:26 +0000

    update dataset generation

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -11,10 +11,10 @@ make -j 8
 
 function generate_lookups() {
     echo "Generating lookups for $1"
-    #[ -f ../data/$1_equality_lookups_20M ] || ./generate ../data/$1 20000000
-    #[ -f ../data/$1_equality_lookups_18M ] || ./generate ../data/$1 18000000
-    #[ -f ../data/$1_equality_lookups_2M ] || ./generate ../data/$1 2000000
-    #[ -f ../data/$1_equality_lookups_1M ] || ./generate ../data/$1 1000000
+    [ -f ../data/$1_equality_lookups_20M ] || ./generate ../data/$1 20000000
+    [ -f ../data/$1_equality_lookups_18M ] || ./generate ../data/$1 18000000
+    [ -f ../data/$1_equality_lookups_2M ] || ./generate ../data/$1 2000000
+    [ -f ../data/$1_equality_lookups_1M ] || ./generate ../data/$1 1000000
     [ -f ../data/$1_equality_lookups_1 ] || ./generate ../data/$1 1
 }
 
@@ -64,4 +64,5 @@ echo "Generating inserts..."
 #generate_inserts books_200M_uint32
 #generate_inserts books_200M_uint64
 #generate_inserts fb_200M_uint64
+
 #generate_inserts poisoned_wiki_ts_200M_uint64
